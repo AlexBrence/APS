75c26932dff3902d872a1bb74943c0a168cab319
AlexBrence <52332168+AlexBrence@users.noreply.github.com>
2020-03-18 07:23:06 -0500

    Add files via upload

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <list>
+#include <chrono>
+#include <fstream>
 
 using namespace std;
 
@@ -13,18 +16,18 @@ node *tail = nullptr;
 bool running = true;
 
 
-node *iskanjePodatka(int key) {
+void iskanjePodatka(int key) {
     node *current = head;
 
     while (current != nullptr && current->key != key) {
         current = current->next;
     }
 
-    if (current->key != key) {
+    if (current == nullptr) {
         cout << "Ni iskanega podatka!" << endl << endl;
     }
     else {
-        return current;
+        cout << "Število " << key << " je v seznamu." << endl << endl;
     }
 }
 
@@ -49,23 +52,23 @@ void vnosZaElt(int key, int inputKey) {
     newNode->key = inputKey;
 
     node* currNode = head;
-    while (currNode->key != key && currNode != nullptr) {
+    while (currNode != nullptr && currNode->key != key) {
         currNode = currNode->next;
     }
-
-    if (currNode->key != key) {
-        cout << "Vnešena vrednost ne obstaja!" << endl << endl;
+    if (currNode == nullptr) {
+        cout << "Podatek ne obstaja!" << endl << endl;
     }
     else {
-        newNode->next = currNode->next;
         newNode->prev = currNode;
+        newNode->next = currNode->next;
         currNode->next = newNode;
+
 
         if (newNode->next != nullptr) {
             newNode->next->prev = newNode;
         }
         else {
-            node *tail = newNode;
+            tail = newNode;
         }
     }
 }
@@ -77,6 +80,7 @@ void vnosZaRepom(int key) {
 
     if (tail != nullptr) {
         tail->next = newNode;
+        newNode->prev = tail;
     }
     else {
         head = newNode;
@@ -86,27 +90,26 @@ void vnosZaRepom(int key) {
 
 void brisanjePodatka(int key) {
     node *deleted = head;
+
     while (deleted->key != key && deleted != nullptr) {
         deleted = deleted->next;
     }
 
-    if (deleted->key != key) {
+    if (deleted == nullptr || (head == nullptr && tail == nullptr)) {
         cout << "Podatek ne obstaja!" << endl << endl;
     }
-    else {
-        if (deleted->prev == nullptr && deleted->next == nullptr) {
+    else if (deleted->prev == nullptr && deleted->next == nullptr) {
             head = nullptr;
             tail = nullptr;
-        }
-
+    }
+    else {
         if (deleted->prev != nullptr) {
             deleted->prev->next = deleted->next;
 
             if (deleted->next != nullptr) {
                 deleted->next->prev = deleted->prev;
             }
-        }
-        else {
+        } else {
             head = deleted->next;
             head->prev = nullptr;
         }
@@ -117,43 +120,171 @@ void brisanjePodatka(int key) {
             if (deleted->prev != nullptr) {
                 deleted->prev->next = deleted->next;
             }
-        }
-
-        else {
+        } else {
             tail = deleted->prev;
             tail->next = nullptr;
         }
-
     }
-
 
     delete deleted;
 }
 
 
 void izpisGlavaRep() {
-    node *tmp = head;
 
-    while (tmp != nullptr) {
-        cout << tmp->key << ", ";
-        tmp = tmp->next;
+    if (head == nullptr && tail == nullptr) {
+        cout << "Seznam je prazen!" << endl << endl;
     }
-    cout << endl << endl;
+    else {
+        node *tmp = head;
 
-    delete tmp;
+        while (tmp != nullptr) {
+            cout << tmp->key << ", ";
+            tmp = tmp->next;
+        }
+        cout << endl << endl;
+
+        delete tmp;
+    }
 }
 
 
 void izpisRepGlava() {
-    node *tmp = tail;
-
-    while (tmp != nullptr) {
-        cout << tmp->key << ", ";
-        tmp = tmp->prev;
+    if (head == nullptr && tail == nullptr) {
+        cout << "Seznam je prazen!" << endl << endl;
     }
-    cout << endl << endl;
+    else {
+        node *tmp = tail;
 
-    delete tmp;
+        while (tmp != nullptr) {
+            cout << tmp->key << ", ";
+            tmp = tmp->prev;
+        }
+        cout << endl << endl;
+
+        delete tmp;
+    }
+}
+
+
+void hitrostTest(int N) {
+    long long sum = 0;
+    int *array = new int[N];
+    int *norArray = new int[N];
+    ofstream datoteka;
+    datoteka.open("hitrostRezultati.txt");
+
+    // EMPTY THE LIST
+    node *tmp = head;
+    while (tmp != nullptr) {
+        brisanjePodatka(tmp->key);
+        tmp = tmp->next;
+    }
+
+    // FILL LIST
+    auto startList = std::chrono::steady_clock::now();
+    list<int> speedyGonzales;
+    for (int i = 1; i <= N; i++) {
+        speedyGonzales.push_front(i);
+    }
+    auto endList = std::chrono::steady_clock::now();
+
+    cout << "ČAS PRI POLNJENJU SEZNAMA(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(endList-startList).count()
+    << endl;
+
+    // SUM OF ALL ELEMENTS IN LIST
+    auto sumListStart = std::chrono::steady_clock::now();
+    for (int i : speedyGonzales) {
+        sum += i;
+    }
+    auto sumListEnd = std::chrono::steady_clock::now();
+
+    cout << "VSOTA SEZNAMA: " << sum << endl;
+    cout << "ČAS PRI SEŠTEVANJU PODATKOV SEZNAMA(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(sumListEnd-sumListStart).count()
+    << endl << endl;
+
+    datoteka << "->Čas pri polnjenju seznama(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(endList-startList).count()
+    << endl;
+    datoteka << "Čas pri seštevanju elt seznama: " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(sumListEnd-sumListStart).count()
+    << endl;
+
+
+    // ARRAY FIRST SPOT INPUT
+    auto arrayFirstStart = std::chrono::steady_clock::now();
+    for (int i = 0; i < N; i++) {
+        if (i == N-1) {
+            array[0] = i+1;
+        }
+        else {
+            array[0] = i + 1;
+            for (int j = i; j >= 0; j--) {
+                array[j+1] = array[j];
+            }
+        }
+    }
+    auto arrayFirstEnd = std::chrono::steady_clock::now();
+
+    cout << "ČAS PRI POLNJENJU POLJA NA 1. MESTO(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(arrayFirstEnd-arrayFirstStart).count()
+    << endl;
+
+    // SUM OF ALL ELEMENTS IN FIRST-SPOT-ARRAY
+    sum = 0;
+    auto sumArrayFirstStart = std::chrono::steady_clock::now();
+    for (int i = 0; i < N; i++) {
+        sum += array[i];
+    }
+    auto sumArrayFirstEnd = std::chrono::steady_clock::now();
+
+    cout << "VSOTA FIRST-SPOT-POLJA JE: " << sum << endl;
+    cout << "ČAS PRI SEŠTEVANJU PODATKOV POLJA(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(sumArrayFirstEnd-sumArrayFirstStart).count()
+    << endl << endl;
+
+    datoteka << "->Čas pri polnjenju first-spot-arraya(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(arrayFirstEnd-arrayFirstStart).count()
+      << endl;
+    datoteka << "Čas pri seštevanju elt: " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(sumArrayFirstEnd-sumArrayFirstStart).count()
+    << endl;
+
+
+    // NORMAL ARRAY FILL
+    auto arrayStart = std::chrono::steady_clock::now();
+    for (int i = 0; i < N; i++) {
+        norArray[i] = i+1;
+    }
+    auto arrayEnd = std::chrono::steady_clock::now();
+
+    cout << "ČAS PRI POLNJENJU NAVADNEGA POLJA(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(arrayEnd-arrayStart).count()
+    << endl;
+
+    // SUM OF ELEMENTS IN NORMAL ARRAY
+    sum = 0;
+    auto sumArrayStart = std::chrono::steady_clock::now();
+    for (int i = 0; i < N; i++) {
+        sum += norArray[i];
+    }
+    auto sumArrayEnd = std::chrono::steady_clock::now();
+
+    cout << "VSOTA PRI SEŠTEVANJU NAVADNEGA POLJA JE: " << sum << endl;
+    cout << "ČAS PRI SEŠTEVANJU PODATKOV NAVADNEGA POLJA(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(sumArrayEnd-sumArrayStart).count()
+    << endl << endl;
+
+    datoteka << "->Čas pri polnjenju navadnega arraya(mikrosek): " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(arrayEnd-arrayStart).count()
+    << endl;
+    datoteka << "Čas pri seštevanju elt: " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(sumArrayEnd-sumArrayStart).count()
+    << endl;
+
+    datoteka.close();
 }
 
 
@@ -214,7 +345,9 @@ void izpisMenija() {
             izpisRepGlava();
             break;
         case 8 :
-            //hitrostTest();
+            cout << "Vnesi količino podatkov: ";
+            cin >> value;
+            hitrostTest(value);
             break;
         case 9 :
             running = false;
@@ -224,6 +357,8 @@ void izpisMenija() {
             break;
     }
 }
+
+
 
 int main() {
 
