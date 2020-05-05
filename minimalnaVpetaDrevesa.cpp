a07ea24399660230f28a13e9cc009356c4a6fddd
AlexBrence <52332168+AlexBrence@users.noreply.github.com>
2020-05-05 10:27:51 +0200

    Add files via upload

--- minimalnaVpetaDrevesa.cpp
+++ minimalnaVpetaDrevesa.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <chrono>
 
 using namespace std;
 
@@ -14,8 +15,11 @@ struct Povezava {
 };
 bool running = true;
 vector<Povezava*> povezave;
+vector<Povezava*> sprejetePovezave;
+int steviloVozlisc;
 
-int partition(const int &dno, const int &vrh) {
+
+int partition(int dno, int vrh) {
     int pe = povezave[0]->cena;
     int l = dno;
     int d = vrh;
@@ -32,7 +36,7 @@ int partition(const int &dno, const int &vrh) {
     return d;
 }
 
-void quickSort(const int &dno, const int &vrh) {
+void quickSort(int dno, int vrh) {
     if (dno < vrh) {
         int j = partition(dno, vrh);
         quickSort(dno, j-1);
@@ -46,9 +50,9 @@ void beri() {
         povezave.clear();
 
     ifstream file("/home/alex/Documents/APS/vaja9/graf.txt");
-    int stVozlisc, stPovezav;
+    int stPovezav;
 
-    file >> stVozlisc;
+    file >> steviloVozlisc;
     file >>stPovezav;
 
     for (int i = 0; i < stPovezav; i++) {
@@ -56,9 +60,15 @@ void beri() {
         file >> tmp->p >> tmp->q >> tmp->cena;
         povezave.emplace_back(tmp);         // &*tmp
     }
+
+    // Če ni urejeno uredi
+    for (int i = 0; i < steviloVozlisc - 1; i++) {
+        if (povezave[i] > povezave[i+1])
+            quickSort(0, povezave.size() - 1);
+    }
 }
 
-void generiraj(const int &steviloVozlisc) {
+void generiraj() {
     if (!povezave.empty())
         povezave.clear();
 
@@ -85,6 +95,87 @@ void generiraj(const int &steviloVozlisc) {
     quickSort(0, povezave.size() - 1);
 }
 
+int poisciMax(const vector<int>& mnozice) {
+    int max = 0;
+    for (const auto &elt : mnozice) {
+        if (elt > max)
+            max = elt;
+    }
+    return max;
+}
+
+vector<Povezava *> kruskal() {
+    auto start = std::chrono::steady_clock::now();
+
+    if (!sprejetePovezave.empty())
+        sprejetePovezave.clear();
+
+    vector<int> mnozice(steviloVozlisc, 0);
+
+    cout << "Število vhodnih vozlišč: " << steviloVozlisc << endl;
+    cout << "Število vhodnih povezav: " << povezave.size() << endl;
+
+    for (auto &elt : povezave) {
+        // Če sta v enaki množici zavrni
+        if ((mnozice[elt->p - 1] == mnozice[elt->q - 1]) && (mnozice[elt->p - 1] != 0 && mnozice[elt->q - 1] != 0))
+            continue;
+
+        // Če nista v enaki množici, ju združi pod eno množico
+        else if ((mnozice[elt->p - 1] != mnozice[elt->q - 1]) && (mnozice[elt->p - 1] != 0 && mnozice[elt->q - 1] != 0)) {
+            for (auto &zdruzi : mnozice) {
+                if (zdruzi == mnozice[elt->q - 1])
+                    zdruzi = mnozice[elt->p - 1];
+            }
+            sprejetePovezave.push_back(elt);
+        }
+
+        // Če eno od vozlišč še ni v množici
+        else if ((mnozice[elt->p - 1] == 0 && mnozice[elt->q - 1] != 0) || (mnozice[elt->q - 1] == 0 && mnozice[elt->p - 1] != 0)) {
+            mnozice[elt->p - 1] == 0 ? mnozice[elt->p - 1] = mnozice[elt->q - 1] : mnozice[elt->q - 1] = mnozice[elt->p - 1];
+            sprejetePovezave.push_back(elt);
+        }
+
+        // Če še nobeno vozlišče ni v množici
+        else if (mnozice[elt->p - 1] == 0 && mnozice[elt->q - 1] == 0) {
+            mnozice[elt->p - 1] = poisciMax(mnozice) + 1;
+            mnozice[elt->q - 1] = mnozice[elt->p - 1];
+            sprejetePovezave.push_back(elt);
+        }
+
+        if (sprejetePovezave.size() == (steviloVozlisc - 1)) {
+            cout << "Število sprejetih povezav je: " << sprejetePovezave.size() << endl;
+
+            auto stop = std::chrono::steady_clock::now();
+            cout << "Čas trajanja algoritma je: " <<
+                std::chrono::duration_cast<std::chrono::microseconds>(stop-start).count()
+            << " mikrosekund";
+            return sprejetePovezave;
+        }
+    }
+
+    cout << "Ni rešitve.";
+
+    auto stop = std::chrono::steady_clock::now();
+    cout << "Čas trajanja algoritma je: " <<
+         std::chrono::duration_cast<std::chrono::microseconds>(stop-start).count()
+     << " mikrosekund";
+}
+
+
+void izpisPovezav() {
+    if (sprejetePovezave.empty()) {
+        cout << "Ni sprejetih povezav!";
+        return;
+    }
+
+    cout << steviloVozlisc << endl;
+    cout << sprejetePovezave.size() << endl;
+
+    for (const auto &elt : sprejetePovezave) {
+        cout << elt->p << " " << elt->q << " " << elt->cena << endl;
+    }
+}
+
 
 void izpisMenija() {
     cout << "Kruskalov algoritem - izbira" << endl << endl;
@@ -96,24 +187,26 @@ void izpisMenija() {
     cout << "Izbira: " << endl;
 
     int choice;
+
     cin >> choice;
     switch (choice) {
         case 1:
             beri();
             break;
         case 2:
-            int stVozlisc;
             cout << "Vnesi število vozlišč (max 1500)" << endl;
-            cin >> stVozlisc;
-            if (stVozlisc < 3 || stVozlisc > 1500) {
+            cin >> steviloVozlisc;
+            if (steviloVozlisc < 3 || steviloVozlisc > 1500) {
                 cout << "Napaka";
             } else {
-                generiraj(stVozlisc);
+                generiraj();
             }
             break;
         case 3:
+            kruskal();
             break;
         case 4:
+            izpisPovezav();
             break;
         case 5:
             running = false;
@@ -126,13 +219,12 @@ void izpisMenija() {
     cout << endl << endl;
 }
 
+
 int main() {
 
     while (running) {
         izpisMenija();
     }
-
-
 
     return 0;
 }
